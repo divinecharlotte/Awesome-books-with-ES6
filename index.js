e956ce67679e9f75e019a2f21b24d5099218ca41
charlotte-divine <divinemaina@gmail.com>
2022-05-30 14:58:18 +0100

    add date

--- index.js
+++ index.js
@@ -1,24 +1,5 @@
 import Book from './modules/books.js';
-
-const handleTime = () => {
-  const dateContainer = document.querySelector('.date-container');
-  const dateFunc = new Date();
-  const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-  const year = dateFunc.getFullYear();
-  const month = months[dateFunc.getMonth()];
-  let currentDate = dateFunc.getDate();
-  const hour = dateFunc.getHours();
-  const minutes = dateFunc.getMinutes();
-  const second = dateFunc.getSeconds();
-  if (currentDate === 1) currentDate = 'first';
-  else if (currentDate === 2) currentDate = 'second';
-  else if (currentDate === 3) currentDate = 'third';
-  else currentDate += 'th';
-
-  const time = (hour < 12) ? `${month} ${currentDate} ${year}, ${hour} ${minutes} ${second} AM`
-    : `${month} ${currentDate} ${year}, ${hour} ${minutes} ${second} PM`;
-  dateContainer.innerHTML = time;
-};
+import handleTime from './modules/menu.js';
 
 window.addEventListener('load', () => {
   handleTime();
@@ -36,3 +17,27 @@ document.querySelector('#book-form').addEventListener('submit', (e) => {
   title.value = '';
   author.value = '';
 });
+
+const list = document.querySelector('#n1');
+const addBook = document.querySelector('#n2');
+const contactUs = document.querySelector('#n3');
+
+list.addEventListener('click', () => {
+  document.getElementById('show-book').style.cssText = 'display: flex;';
+  document.getElementById('book-form').style.cssText = 'display: none;';
+  document.getElementById('show-contact').style.cssText = 'display: none;';
+});
+
+addBook.addEventListener('click', () => {
+  document.getElementById('show-book').style.cssText = 'display: none;';
+  document.getElementById('book-form').style.cssText = 'display: flex;';
+  document.getElementById('show-contact').style.cssText = 'display: none;';
+});
+
+contactUs.addEventListener('click', () => {
+  document.getElementById('show-book').style.cssText = 'display: none;';
+  document.getElementById('book-form').style.cssText = 'display: none;';
+  document.getElementById('show-contact').style.cssText = 'display: flex;';
+});
+
+export default { list, addBook, contactUs };
